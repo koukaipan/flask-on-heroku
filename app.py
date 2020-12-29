0bd795d2a36a771712a0941c3ce8d1b753b62630
Yingshiuan Pan <yingshiuan.pan@gmail.com>
2020-12-29 22:40:31 +0800

    Add a simple form, post, get page
    The /post page receives POST method and print all parameters come through
    POST, and /get page receives GET method.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 import os
-from flask import Flask
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config['KEY'] = os.environ.get('KEY', 'the_default_value_would_be_overwritten')
 
+@app.route('/post', methods=['POST'])
+def post():
+    if request.method == 'POST':
+        result = ''
+        for key, value in request.form.items():
+            result += '%s = %s <br> \n' % (key, value)
+        return 'All variables come from POST: <br> \n' + result
+
+@app.route('/get', methods=['GET'])
+def get():
+    if request.method == 'GET':
+        result = ''
+        for key, value in request.args.items():
+            result += '%s = %s <br> \n' % (key, value)
+        return 'All variables come from GET: <br> \n' + result
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
 @app.route('/')
 def index():
